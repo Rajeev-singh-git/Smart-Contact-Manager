dae7197c73cbb5c7cf5c6de5ce3e56596432bdc3
Rajeev Singh <rajeevsinghece49@gmail.com>
2024-07-25 14:21:24 +0530

    Sending data to View using Model

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -2,14 +2,19 @@ package com.scm.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home(){
-        System.out.println("Home Page");
+    public String home(Model model){
+        System.out.println("Home Page Loaded");
+
+        //sending data to view
+        model.addAttribute("Name", "Rajeev Tech");
+        model.addAttribute("GithubRepo", "https://github.com/Rajeev-singh-git");
         return "home";
     }
 
