cb4bd368a22de602e532e09fb1d1d5ced1b55548
Rajeev Singh <rajeevsinghece49@gmail.com>
2024-07-29 01:19:17 +0530

    Entity Created

---
+++ src/main/java/com/scm/entities/Contact.java
@@ -0,0 +1,30 @@
+package com.scm.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "contact")
+public class Contact {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String picture;
+    @Column(length = 1000)
+    private String description;
+    private boolean favourite = false;
+    private String websiteLink;
+    private String LinkedInLink;
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany(mappedBy = "contact", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    private List<SocialLink> links = new ArrayList<>();
+
+}
